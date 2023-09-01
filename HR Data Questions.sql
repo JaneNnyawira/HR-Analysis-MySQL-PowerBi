ffd7a948805329feeef22a3350014bff75f34e53
JNN <134518125+JaneNnyawira@users.noreply.github.com>
2023-09-01 14:25:10 +0300

    Add files via upload

---
+++ HR Data Questions.sql
@@ -0,0 +1,17 @@
+-- QUESTIONS
+
+-- 1. What is the gender breakdown of employees in the company?
+
+-- 2. What is the race/ethnicity breakdown of employees in the company?
+
+-- 3. What is the age distribution of employees in the company?
+
+-- 4. What is the age disttbution of gender in the company?
+
+-- 5. How many employees work at headquarters versus remote locations?
+
+-- 6. What is the average length of employment for employees who have been terminated?
+
+-- 7. How does the gender distribution vary across departments and job titles?
+
+
